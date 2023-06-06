da1db06b3bc254ca300a4fe90b2afb97f8f08ef8
Julio <julio.mech@hotmail.com>
2023-06-06 09:07:07 -0300

    category get endpoint

--- bookstore/src/main/java/com/julio/bookstore/services/CategoryService.java
+++ bookstore/src/main/java/com/julio/bookstore/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.julio.bookstore.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,13 @@ public class CategoryService {
     public List<Category> findAll() {
 
         return respository.findAll();
+    }
+
+    public Category findById(Long id) {
+
+        Optional<Category> obj = respository.findById(id);
+
+        return obj.get();
     }
 
 }
