6c0850201e485a2bd6a1469303cfe7d7103c05f1
Julio <julio.mech@hotmail.com>
2023-06-11 08:14:23 -0300

    books endpoints

--- bookstore/src/main/java/com/julio/bookstore/services/BookService.java
+++ bookstore/src/main/java/com/julio/bookstore/services/BookService.java
@@ -1,12 +1,14 @@
 package com.julio.bookstore.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.julio.bookstore.domain.Book;
 import com.julio.bookstore.repositories.BookRepository;
+import com.julio.bookstore.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class BookService {
@@ -16,5 +18,12 @@ public class BookService {
 
     public List<Book> findAll() {
         return repository.findAll();
+    }
+
+    public Book findById(Long id) {
+        Optional<Book> obj = repository.findById(id);
+
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado: " + id));
+
     }
 }
